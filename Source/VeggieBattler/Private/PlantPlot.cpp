818d4db7529a0e4deb364c6acbd637c9fce33864
Cmt0080 <cmt0080@auburn.edu>
2026-08-04 21:50:11 -0500

    PlantPlot state machine working

---
+++ Source/VeggieBattler/Private/PlantPlot.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlantPlot.h"
+#include "TimerManager.h"
+
+APlantPlot::APlantPlot()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void APlantPlot::Interact()
+{
+	switch (CurrentState)
+	{
+	case EPlotState::Empty:
+		SetState(EPlotState::Growing);
+		GetWorldTimerManager().SetTimer(
+			GrowTimerHandle,
+			this,
+			&APlantPlot::FinishGrowing,
+			GrowTimeSeconds,
+			false);
+		break;
+
+	case EPlotState::Growing:
+		break;
+
+	case EPlotState::Ready:
+		SetState(EPlotState::Empty);
+		break;
+	}
+}
+
+void APlantPlot::FinishGrowing()
+{
+	SetState(EPlotState::Ready);
+}
+
+void APlantPlot::SetState(EPlotState NewState)
+{
+	if (CurrentState == NewState) return;
+
+	CurrentState = NewState;
+	OnStateChanged(NewState);
+}
---
+++ Source/VeggieBattler/Public/PlantPlot.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PlantPlot.generated.h"
+
+UENUM(BlueprintType)
+enum class EPlotState : uint8
+{
+	Empty		UMETA(DisplayName = "Empty"),
+	Growing		UMETA(DisplayName = "Growing"),
+	Ready		UMETA(DisplayName = "Ready"),
+};
+
+UCLASS(Blueprintable)
+class VEGGIEBATTLER_API APlantPlot : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APlantPlot();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Plant Plot")
+	float GrowTimeSeconds = 5.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Plant Plot")
+	EPlotState CurrentState = EPlotState::Empty;
+
+	UFUNCTION(BlueprintCallable, Category = "Plant Plot")
+	void Interact();
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = "Plant Plot")
+	void OnStateChanged(EPlotState NewState);
+
+private:
+	void SetState(EPlotState NewState);
+	void FinishGrowing();
+
+	FTimerHandle GrowTimerHandle;
+};
